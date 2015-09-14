652710ea955a50b3ddfadf61026cc95750630e22
Mirim <mirim@e-mirim.hs.kr>
2015-09-14 17:14:13 +0900

    add UDPClient address > message

--- UDP/src/UDPServerMain.java
+++ UDP/src/UDPServerMain.java
@@ -1,17 +1,28 @@
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 /**
  * Created by Mirim on 2015-09-10.
  */
 public class UDPServerMain {
     public static void main(String[] args) throws IOException {
-        // printMessage
-        // makePacket
+        byte[] data = new byte[1024];
 
         // makeSocket
-        Socket socket = new ServerSocket(1226).accept();
+        DatagramSocket ds = new DatagramSocket(1226);
+
+        // makePacket
+        DatagramPacket dp = new DatagramPacket(data, data.length);
+
         // receive
+        System.out.println("UDP Server standby..");
+        ds.receive(dp);
+
+        // printMessage
+        String message = new String(dp.getData());
+        System.out.println(dp.getAddress()+" > "+ message);
+
+        // bye 오면 그만 하기
+
     }
 }
