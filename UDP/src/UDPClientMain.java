98d9b47d18774de70a7c4f6311ddfcd2b128665a
Mirim <mirim@e-mirim.hs.kr>
2015-09-15 17:08:13 +0900

    add UDP loop

--- UDP/src/UDPClientMain.java
+++ UDP/src/UDPClientMain.java
@@ -7,25 +7,29 @@ import java.util.Scanner;
  */
 public class UDPClientMain {
     public static void main(String[] args) throws IOException {
-        // inputMessage
-        System.out.println("send Message > ");
         Scanner sc = new Scanner(System.in);
         String message = "";
-        message = sc.nextLine(); // 한줄 읽기
-        System.out.println(message);
 
-        // makePacket
-        DatagramPacket dp = new DatagramPacket(
-                message.getBytes(),                     // 보낼 메세지의 내용
-                message.getBytes().length,              // 보낼 메세지의 길이
-                InetAddress.getByName("localhost"),     // IP 주소
-                1226                                    // 포트번호
-        );
+        while(true) {
+            // inputMessage
+            System.out.print("send Message > ");
+            message = sc.nextLine(); // 한줄 읽기
 
-        // makeSocket
-        DatagramSocket ds = new DatagramSocket();       // 마차 생성..다그닥 다그닥
+            // makePacket
+            DatagramPacket dp = new DatagramPacket(
+                    message.getBytes(),                     // 보낼 메세지의 내용
+                    message.getBytes().length,              // 보낼 메세지의 길이
+                    InetAddress.getByName("localhost"),     // IP 주소
+                    1226                                    // 포트번호
+            );
 
-        // Send
-        ds.send(dp);    // 보냄
+            // makeSocket
+            DatagramSocket ds = new DatagramSocket();       // 마차 생성..다그닥 다그닥
+
+            // Send
+            ds.send(dp);    // 보냄
+
+            if(message.equals("bye")) break;
+        } //while
     }
 }
--- UDP/src/UDPServerMain.java
+++ UDP/src/UDPServerMain.java
@@ -6,23 +6,29 @@ import java.net.*;
  */
 public class UDPServerMain {
     public static void main(String[] args) throws IOException {
-        byte[] data = new byte[1024];
+        byte[] data = null;
+        String message = "";
 
         // makeSocket
-        DatagramSocket ds = new DatagramSocket(1226);
+        DatagramSocket ds = new DatagramSocket(1226); // 뚫어 놓은 것은 하나면 충분함
+        while(true) {
+            data = new byte[1024];
 
-        // makePacket
-        DatagramPacket dp = new DatagramPacket(data, data.length);
+            // makePacket
+            DatagramPacket dp = new DatagramPacket(data, data.length);  // 오는 것 마다 다르니까 새로 만들어줘야돼
 
-        // receive
-        System.out.println("UDP Server standby..");
-        ds.receive(dp);
+            // receive
+            System.out.println("UDP Server standby..");
+            ds.receive(dp);
 
-        // printMessage
-        String message = new String(dp.getData());
-        System.out.println(dp.getAddress()+" > "+ message);
+            // printMessage
+            message = new String(dp.getData()).trim();  //byte 배열이 남은 것을 공백으로 String으로 만들기 때문에 다 빼버림. => 오로지 bye를 추출 하기 위해서
+            System.out.println(dp.getAddress() + " > " + message);
 
-        // bye 오면 그만 하기
-
+            if(message.equals("bye"))  {
+                System.out.println("Server Off");
+                break;
+            }
+        }
     }
 }
