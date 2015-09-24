721950c9e9ccff1ea9044d0b4646252640028745
Mirim <mirim@e-mirim.hs.kr>
2015-09-24 16:50:43 +0900

    add URLMain

---
+++ URL/src/URLMain.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.net.URL;
+
+/**
+ * Created by Mirim on 2015-09-24.
+ */
+public class URLMain {
+    public static void main(String[] args) {
+        // URL
+        URL url = null;
+
+        // make Reader
+        BufferedReader br = null;
+        String line = "";
+        StringBuffer sb = new StringBuffer();
+
+
+        // read
+        // print
+
+
+        // make GUI
+        // pattern matching
+
+    }
+}
